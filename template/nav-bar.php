ff8475f8e62177112b2703defc19e92717095b5d
seasonpaudel <paudelseason@gmail.com>
2023-07-11 21:25:59 +0545

    multiiiiiiiiiiiii

--- template/nav-bar.php
+++ template/nav-bar.php
@@ -1,7 +1,7 @@
 <!-- nav-bar.php -->
 <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
   	<div class="container">
-	    <a class="navbar-brand" href="index.php">Multi Restaurant Table Booking</a>
+	    
 	    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
 	      <span class="oi oi-menu"></span> Menu
 	    </button>

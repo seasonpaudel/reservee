70db7a649f2a75e9b7d40cdcd6a1c9396d669033
Ririii85 <smritigrg22@gmail.com>
2023-09-25 23:08:53 +0545

    sutnai dinna ta

--- reservation.php
+++ reservation.php
@@ -87,7 +87,7 @@ if (!isset($_SESSION['isLoggedIn'])) {
           </div>
         </div>
         <div class="row d-flex">
-        <div class="col-md-4 ftco-animate img" style="background-image: url('<?php echo getRestaurantBackgroundImage($_GET['1']); ?>'); width: 300px; height: 550px;"></div>
+        <div class="col-md-4 ftco-animate img" style="background-image: url('<?php echo getRestaurantBackgroundImage($_GET['res_id']); ?>'); width: 300px; height: 550px;"></div>
           <div class="col-md-8 ftco-animate makereservation p-5 bg-light">
             
             <form action="choose-table.php" method="POST">

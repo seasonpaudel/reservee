e363fe5c9976e45f5bc980f25acd5fb94c3eec16
seasonpaudel <paudelseason@gmail.com>
2023-09-26 15:30:29 +0545

    Rs.

--- book.php
+++ book.php
@@ -126,7 +126,7 @@ if (isset($_POST['confirm'])) {
 		            </div>
 	              	<div class="col mb-3 d-flex py-4 border" style="background: white;">
 		                <div class="align-self-center">
-		                  	<p class="mb-0"><span>Total Price:</span> <a href=""><?php echo $total_price; ?> Rs.</a></p>
+		                  	<p class="mb-0"><span>Total Price:</span> <a href=""><?php echo $total_price; ?>Rs. </a></p>
 		                </div>
 	              	</div>
 	        	</div>

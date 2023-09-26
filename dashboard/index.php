15904490c343d45a6fe0033d686add197216c0ed
Ririii85 <smritigrg22@gmail.com>
2023-09-26 22:05:53 +0545

    okk

--- dashboard/index.php
+++ dashboard/index.php
@@ -84,6 +84,7 @@ if (!isset($_SESSION['isLoggedIn'])) {
 											<td><?php echo $r['booking_time']; ?></td>
 											<td><?php echo $r['bill']; ?>Rs.</td>
 											<td class="center hidden-phone">
+												
 												<?php 
 													$status = $r['status'];
 													if ($status == 0) {

795a8cb18b409205e778946b86356c2df049e526
Ririii85 <smritigrg22@gmail.com>
2023-09-26 11:11:03 +0545

    lol

--- res_booking.sql
+++ res_booking.sql
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `booking_chair`
+-- Table structure for table `booking_table`
 --
 
 CREATE TABLE `booking_chair` (

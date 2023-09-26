b2c1f2fdcd7c8b8ea5052a68d9b97864d21ff1f2
Ririii85 <smritigrg22@gmail.com>
2023-09-26 11:16:45 +0545

    phpp

--- dashboard/PHPMailer-master/PHPMailerAutoload.php
+++ dashboard/PHPMailer-master/PHPMailerAutoload.php
@@ -23,15 +23,15 @@
  */
 function PHPMailerAutoload($classname)
 {
-    //Can't use __DIR__ as it's only in PHP 5.3+
-    $filename = dirname(__FILE__).DIRECTORY_SEPARATOR.'class.'.strtolower($classname).'.php';
+    // Can't use __DIR__ as it's only in PHP 5.3+
+    $filename = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'class.' . strtolower($classname) . '.php';
     if (is_readable($filename)) {
         require $filename;
     }
 }
 
 if (version_compare(PHP_VERSION, '5.1.2', '>=')) {
-    //SPL autoloading was introduced in PHP 5.1.2
+    // SPL autoloading was introduced in PHP 5.1.2
     if (version_compare(PHP_VERSION, '5.3.0', '>=')) {
         spl_autoload_register('PHPMailerAutoload', true, true);
     } else {
@@ -42,10 +42,12 @@ if (version_compare(PHP_VERSION, '5.1.2', '>=')) {
      * Fall back to traditional autoload for old PHP versions
      * @param string $classname The name of the class to load
      */
-    function customAutoload($class) {
-    {
+    function customAutoload($classname) {
         PHPMailerAutoload($classname);
     }
+
+    spl_autoload_register('customAutoload');
 }
+
 }
 
